663fd243a2a77aaeac8df14b9d94d9dac2cb2414
AkashShukla4 <117896950+AkashShukla4@users.noreply.github.com>
2023-03-24 21:22:55 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,6 +1,23 @@
 function calculateMinCost() {
-  //your code here
-  
-  
-  
+  const ropeLstr = getElementById('rope-lengths').value;
+	const ropeLarr = ropeLstr.split(",").map(Number);
+	 const n = ropeLarr.length;
+
+	let pq = [];
+	for (let index = 0; index < n; index++) {
+		pq.push(ropeLarr[index]);
+	}
+	pq.sort(function (a,b) { return a-b; }); 
+
+	let res = 0;
+	while (pq.length > 1) {
+		let one = pq.shift();
+		let tw0 = pq.shift();
+		res += (one + two);
+		pq.push(one + two);
+		
+		pq.sort(function(a,b) {return a-b;});
+	}
+	
+	document.getElementById('result').textContent = res;
 }  
